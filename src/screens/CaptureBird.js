fb4630f7bab7893dcfdebf9d468bad3ecd98c3d0
totimorpa <totimorpa@gmail.com>
2020-10-17 23:12:51 +0200

    CamaraNova

--- src/screens/CaptureBird.js
+++ src/screens/CaptureBird.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Camera from "../components/CameraHandler";
+import Camara2 from "../components/Camera"
 
 export default function () {
   return (
     <>
-      <button onClick={Camera.startCamera()}></button>
-      <button onClick={Camera.takeSnapshot()}></button>
+      {/* <button onClick={Camera.startCamera()}></button>
+      <button onClick={Camera.takeSnapshot()}></button> */}
+      <Camara2></Camara2>
     </>
   );
 }
